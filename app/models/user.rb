0a5826024b6e02c07bcd673cd922fb0b989b0834
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-03-28 10:27:31 +0900

    snsによる新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,19 @@ class User < ApplicationRecord
 
   has_many :sns_credentials
   
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+
+    # userが登録済みであるか
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
 end
